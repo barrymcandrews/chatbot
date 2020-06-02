9911a038deb8f5fdf817854a9303c5ba3555d5e2
Barry McAndrews <bmcandrews3@hotmail.com>
2020-06-02 10:01:24 -0400

    add 'mask_zero=True'

--- chatbot/models.py
+++ chatbot/models.py
@@ -13,17 +13,19 @@ def Chatbot(vocab_length: int, max_context: int, embeddings=None):
         input_dim=vocab_length,
         input_length=max_context,
         weights=embeddings,
-        output_dim=200
+        mask_zero=True,
+        output_dim=300
     )
 
     encoder_input = keras.Input(shape=(max_context,), dtype='int32')
     encoder_embedding = SharedEmbedding(encoder_input)
-    _, state_h, state_c = LSTM(200, return_state=True)(encoder_embedding)
+    _, state_h, state_c = LSTM(300, return_state=True)(encoder_embedding)
 
     decoder_input = keras.Input(shape=(max_context,), dtype='int32')
     decoder_embedding = SharedEmbedding(decoder_input)
-    decoder_lstm, _, _ = LSTM(200, return_state=True, return_sequences=True)(decoder_embedding, initial_state=[state_h, state_c])
+    decoder_lstm, _, _ = LSTM(300, return_state=True, return_sequences=True)(decoder_embedding, initial_state=[state_h, state_c])
     # decoder_dense = Dense(vocab_length, tf.keras.activations.softmax)(decoder_lstm)
+    # middle = Dense(300, tf.keras.activations.relu)(decoder_lstm)
     decoder_dense = TimeDistributed(Dense(vocab_length, tf.keras.activations.softmax))(decoder_lstm)
 
     return Model(inputs=[encoder_input, decoder_input], outputs=decoder_dense)
