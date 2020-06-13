22eef08cd451ddfba3ec9df1214a6add91db2645
Barry McAndrews <bmcandrews3@hotmail.com>
2020-06-13 09:36:27 -0400

    remove dropout laters

--- chatbot/models.py
+++ chatbot/models.py
@@ -21,7 +21,7 @@ def Chatbot(vocab_length: int, max_context: int, embeddings=None):
 
     encoder_input = keras.Input(shape=(max_context,), dtype='int32', name='encoder_input')
     encoder_embedding = SharedEmbedding(encoder_input)
-    # encoder_embedding_dropout = Dropout(0.3)(encoder_embedding)
+
     encoder_1_states  = Bidirectional(LSTM(lstm_units, return_state=True, return_sequences=True))(encoder_embedding)
     encoder_2_states = Bidirectional(LSTM(lstm_units, return_state=True, return_sequences=True))(encoder_1_states[0])
     encoder_3_states = LSTM(lstm_units, return_state=True, return_sequences=True)(encoder_2_states[0])
@@ -29,12 +29,12 @@ def Chatbot(vocab_length: int, max_context: int, embeddings=None):
 
     decoder_input = keras.Input(shape=(max_context,), dtype='int32', name='decoder_input')
     decoder_embedding = SharedEmbedding(decoder_input)
-    # decoder_embedding_dropout = Dropout(0.3)(decoder_embedding)
+
     decoder_1_states = Bidirectional(LSTM(lstm_units, return_state=True, return_sequences=True))(decoder_embedding, initial_state=encoder_1_states[1:])
     decoder_2_states = Bidirectional(LSTM(lstm_units, return_state=True, return_sequences=True))(decoder_1_states[0], initial_state=encoder_2_states[1:])
     decoder_3_states = Bidirectional(LSTM(lstm_units, return_state=True, return_sequences=True))(decoder_2_states[0], initial_state=encoder_3_states[1:])
     decoder_4_out = LSTM(lstm_units, return_sequences=True)(decoder_3_states[0], initial_state=encoder_4_states[1:])
-    # decoder_lstm_dropout = Dropout(0.5)(decoder_lstm)
+
     decoder_dense = TimeDistributed(Dense(vocab_length, tf.keras.activations.softmax))(decoder_4_out)
 
     return Model(inputs=[encoder_input, decoder_input], outputs=decoder_dense)
