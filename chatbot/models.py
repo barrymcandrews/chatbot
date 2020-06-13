1cca808038b25546d128902b71f474a19062982a
Barry McAndrews <bmcandrews3@hotmail.com>
2020-06-13 09:30:49 -0400

    add more LSTM layers

--- chatbot/models.py
+++ chatbot/models.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 
 from tensorflow import keras
 from tensorflow.keras import layers, Model
-from tensorflow.keras.layers import Embedding, LSTM, Dense, TimeDistributed
+from tensorflow.keras.layers import Embedding, LSTM, Dense, TimeDistributed, Dropout, Bidirectional
 
 import argparse
 import os
@@ -17,13 +17,24 @@ def Chatbot(vocab_length: int, max_context: int, embeddings=None):
         output_dim=300
     )
 
-    encoder_input = keras.Input(shape=(max_context,), dtype='int32')
-    encoder_embedding = SharedEmbedding(encoder_input)
-    _, state_h, state_c = LSTM(300, return_state=True)(encoder_embedding)
+    lstm_units=128
 
-    decoder_input = keras.Input(shape=(max_context,), dtype='int32')
+    encoder_input = keras.Input(shape=(max_context,), dtype='int32', name='encoder_input')
+    encoder_embedding = SharedEmbedding(encoder_input)
+    # encoder_embedding_dropout = Dropout(0.3)(encoder_embedding)
+    encoder_1_states  = Bidirectional(LSTM(lstm_units, return_state=True, return_sequences=True))(encoder_embedding)
+    encoder_2_states = Bidirectional(LSTM(lstm_units, return_state=True, return_sequences=True))(encoder_1_states[0])
+    encoder_3_states = LSTM(lstm_units, return_state=True, return_sequences=True)(encoder_2_states[0])
+    encoder_4_states = LSTM(lstm_units, return_state=True, return_sequences=True)(encoder_3_states[0])
+
+    decoder_input = keras.Input(shape=(max_context,), dtype='int32', name='decoder_input')
     decoder_embedding = SharedEmbedding(decoder_input)
-    decoder_lstm, _, _ = LSTM(300, return_state=True, return_sequences=True)(decoder_embedding, initial_state=[state_h, state_c])
-    decoder_dense = TimeDistributed(Dense(vocab_length, tf.keras.activations.softmax))(decoder_lstm)
+    # decoder_embedding_dropout = Dropout(0.3)(decoder_embedding)
+    decoder_1_states = Bidirectional(LSTM(lstm_units, return_state=True, return_sequences=True))(decoder_embedding, initial_state=encoder_1_states[1:])
+    decoder_2_states = Bidirectional(LSTM(lstm_units, return_state=True, return_sequences=True))(decoder_1_states[0], initial_state=encoder_2_states[1:])
+    decoder_3_states = Bidirectional(LSTM(lstm_units, return_state=True, return_sequences=True))(decoder_2_states[0], initial_state=encoder_3_states[1:])
+    decoder_4_out = LSTM(lstm_units, return_sequences=True)(decoder_3_states[0], initial_state=encoder_4_states[1:])
+    # decoder_lstm_dropout = Dropout(0.5)(decoder_lstm)
+    decoder_dense = TimeDistributed(Dense(vocab_length, tf.keras.activations.softmax))(decoder_4_out)
 
     return Model(inputs=[encoder_input, decoder_input], outputs=decoder_dense)
